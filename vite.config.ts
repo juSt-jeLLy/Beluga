66d2e0da11ba122dcd5d4be4325e13072d35cc2d
juSt-jeLLy <yagneshpatel931@gmail.com>
2025-12-01 16:54:31 +0530

    IP registration

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@ export default defineConfig(({ mode }) => ({
   },
   define: {
     'process.env': '{}',
-    'process.cwd': 'function(){return"/"}',
     'process.platform': '"browser"',
     'process.version': '"v16.0.0"',
     global: 'globalThis',
